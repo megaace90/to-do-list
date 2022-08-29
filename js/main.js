63875490d744f52339422c6b0d6cd7f67c91f792
megaace90 <94745441+megaace90@users.noreply.github.com>
2022-08-29 23:53:57 +1000

    первая рабочая версия

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@ const taskInput = document.querySelector('#add-task__input');
 const tasksList = document.querySelector('.to-do-list');
 const emptyList = document.querySelector('.to-do-list__empty');
 
+let tasks = [];
 
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.forEach((task) => renderTask(task));
+}
+
+
+checkEmptyList();
 
 form.addEventListener('submit', addTask);
 
@@ -18,10 +26,77 @@ tasksList.addEventListener('click', doneTask);
 function addTask (event) {
     event.preventDefault();
     const taskText = taskInput.value;
+    // описываем структуру объектов, в которых будет хранится каждая добавляемая в массив задача
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        done: false,
+    };
+    // добавляем новую задачу в массив
+    tasks.push(newTask);
+    saveToLocalStorage();
+    renderTask(newTask);
+    // очищаем поле ввода и возвращаем на него фокус
+    taskInput.value = "";
+    taskInput.focus();
+    checkEmptyList();
+}
+
+function deleteTask (event) {
+    // проверяем что клик был не по кнопке "удалить задачу"
+    if (event.target.dataset.action !== "delete") return;
+    // находим элемент списка с удаляемой задачей
+    const parentNode = event.target.closest('.to-do-list__item');
+    // определяем id задачи
+    const id = Number(parentNode.id);
+    //удаляем элемент из массива с задачами через фильтрацию массива
+    tasks = tasks.filter((task) => task.id !== id);
+    saveToLocalStorage();
+    parentNode.remove();
+    checkEmptyList();
+}
+
+function doneTask (event) {
+    if (event.target.dataset.action !== "done") return;
+    // находим элемент списка с удаляемой задачей
+    const parentNode = event.target.closest('.to-do-list__item');
+    const id = Number(parentNode.id);
+    const task = tasks.find((task) => task.id === id);
+    task.done = !task.done;
+    saveToLocalStorage();
+    parentNode.classList.toggle('to-do-list__item_done');
+}
+
+function checkEmptyList() {
+    if (tasks.length === 0) {
+        const emptyListHTML = 
+        `<li class="to-do-list__empty">
+            <p class="to-do-list__empty-text">Здесь ничего нет!<br>Чтобы добавить задачу на сегодня, введите текст в поле ниже и нажмите "добавить"</p>
+        </li>`;
+    tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
+    } else {
+        //находим блок "список задач пуст на странице и если он найден, тогда удаляем его"
+        const emptyListEl = document.querySelector('.to-do-list__empty');
+        emptyListEl ? emptyListEl.remove() : null;
+    }
+
+
+
+
+    
+}
+
+function saveToLocalStorage () {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+//рендерим задачу на странице
+function renderTask (task) {
+    // формируем css class для того чтобы отражать на странице выполнена задача или нет
+    const cssClass = task.done ? "to-do-list__item to-do-list__item_done" : "to-do-list__item";
     // генерируем разметку для задачи
     const taskHTML = `
-        <li class="to-do-list__item">
-            <span class="to-do-list__text">${taskText}</span>
+        <li id = "${task.id}" class="${cssClass}">
+            <span class="to-do-list__text">${task.text}</span>
             <button class="to-do-list__button-done button" type="button" data-action="done" id="button-done">
                 <img src="img/done.svg" alt="">
             </button>
@@ -31,31 +106,4 @@ function addTask (event) {
         </li>`;
         // добаляем задачу на страницу
         tasksList.insertAdjacentHTML('beforeend', taskHTML);
-        // очищаем поле ввода и возвращаем на него фокус
-        taskInput.value = "";
-        taskInput.focus();
-        // при добавлении хотя бы одной задачи, удаляем надпись "список задач пуст"
-        if (tasksList.children.length > 1) {
-            emptyList.classList.add('none');
-        }
 }
-
-function deleteTask (event) {
-    // проверяем что клик был не по кнопке "удалить задачу"
-    if (event.target.dataset.action !== "delete") return;
-    // находим элемент списка с удаляемой задачей
-    const parentNode = event.target.closest('.to-do-list__item');
-    parentNode.remove();
-    // при удалении всех задач, возвращаем надпись "список задач пуст"
-    if (tasksList.children.length === 1) {
-        emptyList.classList.remove('none');
-    }
-}
-
-function doneTask (event) {
-    if (event.target.dataset.action !== "done") return;
-    // находим элемент списка с удаляемой задачей
-    const parentNode = event.target.closest('.to-do-list__item');
-    parentNode.classList.toggle('to-do-list__item_done');
-}
-
